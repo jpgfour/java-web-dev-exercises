9f73439657d46d93013720aac74d1654fba26c1e
jpgfour <jeremy.grise@gmail.com>
2020-05-08 16:50:43 -0500

    7.8 Exercises: Inheritance

---
+++ src/exercises/technology/Computer.java
@@ -0,0 +1,81 @@
+package exercises.technology;
+
+public class Computer {
+
+    private String name;
+    private String manufacturer;
+    private String model;
+    private String operatingSystem;
+    private boolean powered;
+    private boolean online;
+
+    //constructor(s)
+    public Computer(String name, String manufacturer, String model, String operatingSystem) {
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.operatingSystem = operatingSystem;
+    }
+
+
+    //getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String aName) {
+        this.name = aName;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String aManufacturer) {
+        this.manufacturer = aManufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String aModel) {
+        this.model = aModel;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public void setOperatingSystem(String aOperatingSystem) {
+        this.operatingSystem = aOperatingSystem;
+    }
+
+    public boolean isPowered() {
+        return powered;
+    }
+
+    public void setPowered(boolean aPowered) {
+        this.powered = aPowered;
+    }
+
+    public boolean isOnline() {
+        return online;
+    }
+
+    public void setOnline(boolean aOnline) {
+        this.online = aOnline;
+    }
+
+    //instance methods
+    public void bootUp() {
+        powered = true;
+        online = true;
+    }
+
+    public void shutDown() {
+        powered = false;
+        online = false;
+    }
+
+}
