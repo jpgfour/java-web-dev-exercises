22ebac196481d762f8e633b12664cf350affdade
jpgfour <jeremy.grise@gmail.com>
2020-05-11 12:14:32 -0500

    7.8 Exercises: Inheritance

--- src/exercises/technology/Laptop.java
+++ src/exercises/technology/Laptop.java
@@ -3,11 +3,60 @@ package exercises.technology;
 public class Laptop extends Computer {
 
     private boolean pluggedIn;
-    private double batteryLifeHours;
+    private double maxBatteryLifeHours;
+    private double currentBatteryLifeHours;
+    private int screenSize;
     private double percentCharged;
 
-    public Laptop(String name, String manufacturer, String model, String operatingSystem) {
-        super(name, manufacturer, model, operatingSystem);
+    public Laptop(String aName, String aManufacturer, String aModel, String aOperatingSystem, int aScreenSize, double aMaxBatteryLifeHours) {
+        super(aName, aManufacturer, aModel, aOperatingSystem);
+        maxBatteryLifeHours = aMaxBatteryLifeHours;
+        screenSize = aScreenSize;
     }
 
+    public boolean isPluggedIn() {
+        return pluggedIn;
+    }
+
+    public void setPluggedIn(boolean aPluggedIn) {
+        this.pluggedIn = aPluggedIn;
+    }
+
+    public double getMaxBatteryLifeHours() {
+        return maxBatteryLifeHours;
+    }
+
+    public void setMaxBatteryLifeHours(double aMaxBatteryLifeHours) {
+        this.maxBatteryLifeHours = aMaxBatteryLifeHours;
+    }
+
+    public double getCurrentBatteryLifeHours() {
+        return currentBatteryLifeHours;
+    }
+
+    public void setCurrentBatteryLifeHours(double aCurrentBatteryLifeHours) {
+        this.currentBatteryLifeHours = aCurrentBatteryLifeHours;
+    }
+
+    public int getScreenSize() {
+        return screenSize;
+    }
+
+    public void setScreenSize(int aScreenSize) {
+        this.screenSize = aScreenSize;
+    }
+
+    public double getPercentCharged() {
+        return percentCharged;
+    }
+
+    public void setPercentCharged(double aPercentCharged) {
+        this.percentCharged = aPercentCharged;
+    }
+
+    public void chargeUp() {
+        this.setPluggedIn(true);
+        this.currentBatteryLifeHours = maxBatteryLifeHours;
+        this.percentCharged = 100;
+    }
 }
---
+++ src/exercises/technology/Smartphone.java
@@ -0,0 +1,7 @@
+package exercises.technology;
+
+public class Smartphone extends Computer{
+    public Smartphone(String name, String manufacturer, String model, String operatingSystem) {
+        super(name, manufacturer, model, operatingSystem);
+    }
+}
